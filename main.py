a423010431a41ae1f7bea30fc96d5777a9d492bb
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-05-08 10:06:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
-
-    # Task1
+# Task1
 
 def oops():
     raise IndexError
+
+
 def doomed():
     try:
         oops()
@@ -11,10 +12,23 @@ def doomed():
         print('caught an index error!')
     else:
         print('no error caught...')
-if __name__ == '__main__': doomed()
+
+
+if __name__ == '__main__':
+    doomed()
+
 
 # Task2
-
-
-
-
+while True:
+    try:
+        a = int(input('Enter a:'))
+        b = int(input('Enter b:'))
+        c = a**2/b
+        print("a**2/b = %d" % c)
+    except ValueError:
+        print('No valid integer! Please try again ...')
+    except Exception:
+        print("can't divide by zero")
+        print(Exception)
+    else:
+        print('Hi I am else block')
